8d80b2c0912b58141f33327b9d404911c7841a06
ebook2025 <ebook2025beleza@gmail.com>
2025-05-16 19:40:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function comprar() {
   alert("Você será redirecionado para o pagamento.");
-  window.location.href = "https://seulinkdepagamento.com"; // Substitua pelo link real do seu checkout
+  window.location.href = "https://pay.kirvano.com/e1006de3-aac5-4d83-91c9-87743f507238"; // Substitua pelo link real do seu checkout
 }
