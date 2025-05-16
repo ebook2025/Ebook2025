ae8e8a3985f70d71f29992de76c784cc228853db
ebook2025 <ebook2025beleza@gmail.com>
2025-05-16 11:10:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function comprar() {
+  alert("Você será redirecionado para o pagamento.");
+  window.location.href = "https://seulinkdepagamento.com"; // Substitua pelo link real do seu checkout
+}
